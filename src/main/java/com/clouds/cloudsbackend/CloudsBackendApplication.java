bdf9ede7dc080fe276acd4b47d4d29d3e4de50d6
apurvathorat <apurva.thorat@wrxflo.com>
2021-10-21 14:20:22 +0100

    initial commit

---
+++ src/main/java/com/clouds/cloudsbackend/CloudsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clouds.cloudsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clouds/cloudsbackend/CloudsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouds.cloudsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
